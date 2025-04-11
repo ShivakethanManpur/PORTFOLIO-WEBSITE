24365f56cdaae941b07fc82d52eddcbcd5900fce
ShivakethanManpur <shivakethanmanpur@gmail.com>
2025-04-11 17:03:42 +0530

    second deploy

---
+++ JS/myscript.js
@@ -0,0 +1,33 @@
+AOS.init();
+
+
+AOS.init({
+  offset: 120,
+  delay: 0, 
+  duration: 900, 
+  easing: 'ease',
+  once: false, 
+  mirror: false, 
+  anchorPlacement: 'top-bottom',
+
+});
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
